5f9180e7b982a857c76f3659dd769def7dc8e268
Leo Dion <leogdion@brightdigit.com>
2020-12-17 07:36:10 -0500

    working on ui

--- eggseed-mac-app/ContentView.swift
+++ eggseed-mac-app/ContentView.swift
@@ -6,12 +6,67 @@
 //
 
 import SwiftUI
+import EggSeedKit
 
+extension License : Identifiable {
+  public var id: String {
+    return self.rawValue
+  }
+  
+  
+}
+
+enum Topping: String, CaseIterable, Identifiable {
+    case nuts
+    case cookies
+    case blueberries
+
+    var id: String { self.rawValue }
+}
+
+extension ContinuousIntegrationSystem : Identifiable {
+  public var id: Int {
+    return self.rawValue
+  }
+}
+
+class URLFormatter : Formatter {
+  override func string(for obj: Any?) -> String? {
+    guard let url = obj as? URL else {
+      return nil
+    }
+    
+    return url.absoluteString
+  }
+  
+  override func getObjectValue(_ obj: AutoreleasingUnsafeMutablePointer<AnyObject?>?, for string: String, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
+    guard let url = URL(string: string) else {
+      return false
+    }
+    
+    obj?.pointee = url as AnyObject
+    return true
+  }
+}
 struct ContentView: View {
+  
+  @State var topping : Topping = .nuts
     @Binding var document: eggseed_mac_appDocument
 
     var body: some View {
-        TextEditor(text: $document.text)
+      VStack{
+        TextField("Template", value: $document.templateURL, formatter: URLFormatter())
+        Picker("License", selection: self.$topping) {
+          ForEach(Topping.allCases) { (topping) in
+            Text(topping.rawValue)
+          }
+        }
+        
+        ForEach(0..<ContinuousIntegrationSystem.allCases.count){
+          Toggle(ContinuousIntegrationSystem.allCases[$0].description, isOn: $document.ci[$0])
+        }
+        Text(self.topping.rawValue)
+      }
     }
 }
 
--- eggseed-mac-app/eggseed_mac_appDocument.swift
+++ eggseed-mac-app/eggseed_mac_appDocument.swift
@@ -6,34 +6,47 @@
 //
 
 import SwiftUI
+import EggSeedKit
 import UniformTypeIdentifiers
 
 extension UTType {
-    static var exampleText: UTType {
-        UTType(importedAs: "com.example.plain-text")
-    }
+  static var exampleText: UTType {
+    UTType(importedAs: "com.example.plain-text")
+  }
 }
 
 struct eggseed_mac_appDocument: FileDocument {
-    var text: String
-
-    init(text: String = "Hello, world!") {
-        self.text = text
+  public var license: License
+  var ci: [Bool] = [Bool].init(repeating: true, count: ContinuousIntegrationSystem.allCases.count)
+  
+  var komondor: Bool = true
+  var rocket: Bool = true
+  public var templateURL: URL = URL(string: "https://github.com/brightdigit/eggseed-template/archive/master.zip")!
+  
+  // sourcedocs or jazzy
+  var documentation: DocumentationTooling = .sourcedocs
+  
+  // swiftformat or/and swiftlint danger etc...
+  public var linters: StylingToolOption = .all
+  public var packageType: SwiftPackageType = .library
+  public var userName: String?
+  init() {
+    self.license = .mit
+  }
+  
+  static var readableContentTypes: [UTType] { [.exampleText] }
+  
+  init(configuration: ReadConfiguration) throws {
+    guard let data = configuration.file.regularFileContents,
+          let string = String(data: data, encoding: .utf8)
+    else {
+      throw CocoaError(.fileReadCorruptFile)
     }
-
-    static var readableContentTypes: [UTType] { [.exampleText] }
-
-    init(configuration: ReadConfiguration) throws {
-        guard let data = configuration.file.regularFileContents,
-              let string = String(data: data, encoding: .utf8)
-        else {
-            throw CocoaError(.fileReadCorruptFile)
-        }
-        text = string
-    }
-    
-    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
-        let data = text.data(using: .utf8)!
-        return .init(regularFileWithContents: data)
-    }
+    self.license = .mit
+  }
+  
+  func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+    let data = "".data(using: .utf8)!
+    return .init(regularFileWithContents: data)
+  }
 }
